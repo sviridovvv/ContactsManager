de88265bbd1537f22c9174b357233e28b16e3093
sviridov <sviridov@bifit.com>
2022-10-22 21:24:41 +0600

    creating a screen of AddNewContactVC

--- ContactsManager/Controller/AddNewContactVC.swift
+++ ContactsManager/Controller/AddNewContactVC.swift
@@ -9,6 +9,48 @@ import UIKit
 
 class AddNewContactVC: UIViewController {
 
+    @IBOutlet var firstNameLabel: UILabel! {
+        didSet {
+            firstNameLabel.text = "First name"
+            firstNameLabel.font = AppFonts.labelFont
+        }
+    }
+
+    @IBOutlet var lastNameLabel: UILabel! {
+        didSet {
+            lastNameLabel.text = "Last name"
+            lastNameLabel.font = AppFonts.labelFont
+        }
+    }
+
+    @IBOutlet var phoneNumberLabel: UILabel! {
+        didSet {
+            phoneNumberLabel.text = "Phone number"
+            phoneNumberLabel.font = AppFonts.labelFont
+        }
+    }
+
+    @IBOutlet var emailLabel: UILabel! {
+        didSet {
+            emailLabel.text = "Email"
+            emailLabel.font = AppFonts.labelFont
+        }
+    }
+
+    @IBOutlet var addButton: UIButton! {
+        didSet {
+            addButton.setTitle("Add contact", for: .normal)
+            addButton.setTitleColor(.black, for: .normal)
+            addButton.setTitleColor(.gray, for: .highlighted)
+            addButton.titleLabel?.font = AppFonts.buttonFont
+            addButton.layer.cornerRadius = 5
+            addButton.layer.borderWidth = 1
+        }
+    }
+
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- ContactsManager/Resources/Fonts.swift
+++ ContactsManager/Resources/Fonts.swift
@@ -10,4 +10,6 @@ import UIKit
 public enum AppFonts {
     public static let titleFont = UIFont.systemFont(ofSize: 20, weight: .medium)
     public static let cellFont = UIFont.systemFont(ofSize: 16, weight: .regular)
+    public static let labelFont = UIFont.systemFont(ofSize: 14, weight: .regular)
+    public static let buttonFont = UIFont.systemFont(ofSize: 18, weight: .semibold)
 }
